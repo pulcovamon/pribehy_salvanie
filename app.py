fc5ff6b2e8d6077558d32c62923cae3b4d7b6ae8
Monika <pulcovamon@gmail.com>
2023-02-03 19:57:06 +0100

    mobile phone compatibility

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
---
+++ static/dropdown.js
@@ -0,0 +1,10 @@
+function gfgMenu() {
+    const GFG = document.querySelector('.links');
+  
+    if (GFG.style.display === "none") {
+        GFG.style.display = "block";
+    }
+    else {
+        GFG.style.display = "none";
+    }
+}
